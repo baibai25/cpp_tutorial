1cae63ef6fa30e43b0ce65214369312d9d7920e0
baibai25 <kamuy.camel.0225@gmail.com>
2019-03-01 22:58:41 +0900

    First commit

---
+++ src/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Define namespace
+using namespace std;
+ 
+int main(){
+    /* 
+     * printf()
+     * cout: iostream's object
+     * endl == \n
+     */ 
+    cout << "Hello, world!" << endl;
+    cout<<"Hello C++\n";
+    
+    // namespace::class, variables etc...
+    //std::cout << "Helloooo, world!" << std::endl;
+
+    return 0;
+}
